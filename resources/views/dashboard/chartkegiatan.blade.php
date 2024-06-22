6adc223e5b78c10cea61dbaf6ecd407126cd7ec1
blimika <iputudyatmika@gmail.com>
2024-06-22 16:33:13 +0800

    v2.5.8
    perbaikan dashboard depan

--- resources/views/dashboard/chartkegiatan.blade.php
+++ resources/views/dashboard/chartkegiatan.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-body">
-        <h4 class="card-title">Progress Jumlah kegiatan Perbulan Tahun {{$tahun_berjalan}}</h4>
+        <h4 class="card-title">Progress Jumlah kegiatan Perbulan Tahun {{$tahun_berjalan-2}} - {{$tahun_berjalan}}</h4>
         <div class="text-center">
             <div id="kegiatan-depan"></div>
         </div>
